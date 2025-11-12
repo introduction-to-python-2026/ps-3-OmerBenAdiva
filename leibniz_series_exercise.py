3adb16160af5c43a657fdcfcaa04f19b347ead85
OmerBenAdiva <25omer256@gmail.com>
2025-11-12 22:07:38 +0200

    Update leibniz_series_exercise.py סופי

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,17 +1,7 @@
 def approximate_pi(n_terms):
-     leibniz_series = []
+    apx_pi = 0
+    for i in range(n_terms):
+        apx_pi += 4 * ((-1) ** i / (2 * i + 1))
+    return apx_pi
 
-    # מוסיפים לרשימה את כל האיברים לפי הנוסחה
-    for n in range(n_terms):
-        term = ((-1) ** n) / (2 * n + 1)
-        leibniz_series.append(term)
-
-    # מחשבים את סכום כל האיברים
-    total = sum(leibniz_series)
-
-    # נכפיל ב-4 כדי לקבל קירוב לערך של π
-    pi_approx = 4 * total
-
-    # נחזיר את התוצאה
-    return pi_approx
 
